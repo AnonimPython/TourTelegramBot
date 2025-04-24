89918e86059da234545e518b123cc7a86b69c8af
AnonimPython <moscow.retro@list.ru>
2025-04-24 23:40:31 +0300

    add bot entry point and configuration

--- main.py
+++ main.py
@@ -2,5 +2,23 @@ import dotenv
 import os
 dotenv.load_dotenv()
 
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from app.handlers import register_handlers
+import asyncio
+
+
 
 TOKEN = os.getenv("TOKEN")
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher(storage=MemoryStorage())
+    
+    register_handlers(dp)
+    
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
